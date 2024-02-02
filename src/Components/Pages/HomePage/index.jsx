bdf75a960b8d3f837b8fc67f76089737f299bf56
marciomartins12 <mmarciom1martins635@gmail.com>
2024-02-02 15:56:07 -0300

    init homepage

---
+++ src/Components/Pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import Style from "./homepage.module.css";
+
+const HomePage= ()=>{
+    return (
+        <>
+        testetestesteste
+        </>
+    )
+};
+
+export default HomePage;
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,10 +1,16 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./Components/Pages/HomePage"
 function Rotas() {
 
   return (
-    <>
-     aaaaaa
-    </>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={
+      <HomePage/>
+    }/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
