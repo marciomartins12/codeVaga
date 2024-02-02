38a21b30e5335dcad6bc83daa52de5dce916ed55
marciomartins12 <mmarciom1martins635@gmail.com>
2024-02-02 16:05:54 -0300

    add reset css

--- src/Components/Pages/HomePage/index.jsx
+++ src/Components/Pages/HomePage/index.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import Style from "./homepage.module.css";
 
 const HomePage= ()=>{
     return (
         <>
-        testetestesteste
+        <nav>
+            <Link><img src="" alt="logo da escola"/></Link>
+
+            <Link to="/login">Login</Link>
+            <Link to="/register">Cadastrar</Link>
+        </nav>
         </>
     )
 };
