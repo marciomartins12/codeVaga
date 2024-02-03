db0f810c7511488021713cab8bf24a906f73f000
marciomartins12 <mmarciom1martins635@gmail.com>
2024-02-03 14:53:36 -0300

    page l

--- src/Components/Pages/PageLogin/index.jsx
+++ src/Components/Pages/PageLogin/index.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 import Style from "./loginpage.module.css";
+import { useState } from "react";
 
 const LoginPage = () => {
+    const [troca, settroca] = useState(false);
+    
+    const trocar = () => {
+       
+        settroca(!troca);
+        
+    }
     return (
         <>
-            <div className={Style.container} id="container">
-                <div className={`${Style.formcontainer} ${Style.singnup}`}>
+            <div className={`${Style.container} ${troca == true? Style.active: ""}`} id="container">
+
+                <div className={`${Style.formcontainer} ${Style.signup}}`}>
                     <form>
                         <h2>Create Account</h2>
                         <div className={Style.socialicons}>
@@ -21,7 +30,8 @@ const LoginPage = () => {
                         <button>Sign Up</button>
                     </form>
                 </div>
-                <div className={`${Style.formcontainer} ${Style.singnup}`}>
+
+                <div className={`${Style.formcontainer} ${Style.signin}`}>
                     <form>
                         <h1>Sign In</h1>
                         <div className={Style.socialicons}>
@@ -37,17 +47,19 @@ const LoginPage = () => {
                         <button>Sign In</button>
                     </form>
                 </div>
-                <div className={Style.togglecontainer}>
+
+
+                <div className={`${Style.togglecontainer}`}>
                     <div className={Style.toggle}>
                         <div className={`${Style.togglepanel} ${Style.toggleleft}`}>
                             <h1>Welcome Back!</h1>
                             <p>Enter your personal details to use all of site features</p>
-                            <button className={Style.hidden} id="login">Sign In</button>
+                            <button className={Style.hidden} onClick={()=>trocar()}>Sign In</button>
                         </div>
                         <div className={`${Style.togglepanel} ${Style.toggleright}`}>
                             <h1>Hello, Friend!</h1>
                             <p>Register with your personal details to use all of site features</p>
-                            <button className={Style.hidden} id="register">Sign Up</button>
+                            <button className={Style.hidden} onClick={()=> trocar()} id="register">Sign Up</button>
                         </div>
                     </div>
                 </div>
