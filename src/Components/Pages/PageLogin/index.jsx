c0629a1f5521d462e0d2e554a8edd0fcd6c6da7c
marciomartins12 <mmarciom1martins635@gmail.com>
2024-02-03 16:05:25 -0300

    page login screen pc

--- src/Components/Pages/PageLogin/index.jsx
+++ src/Components/Pages/PageLogin/index.jsx
@@ -1,50 +1,51 @@
-import { Link } from "react-router-dom";
+
 import Style from "./loginpage.module.css";
 import { useState } from "react";
 
 const LoginPage = () => {
     const [troca, settroca] = useState(false);
-    
+    const [typeOfConta, setTypeOfConta] = useState(null);
     const trocar = () => {
-       
         settroca(!troca);
-        
+    }
+    const trocaTipoDeConta = (tipoDeConta) => {
+        setTypeOfConta(tipoDeConta);
+        console.log(tipoDeConta)
     }
     return (
         <>
             <div className={`${Style.container} ${troca == true? Style.active: ""}`} id="container">
 
-                <div className={`${Style.formcontainer} ${Style.signup}}`}>
+                <div className={`${Style.formcontainer} ${Style.signup}`}>
                     <form>
-                        <h2>Create Account</h2>
-                        <div className={Style.socialicons}>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-google-plus-g"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-facebook-f"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-github"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your email for registeration</span>
+                        <h2>Cadastrar Aluno</h2>
+                
                         <input type="text" placeholder="Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
-                        <button>Sign Up</button>
+                        <button>Entre em contato</button>
                     </form>
                 </div>
 
                 <div className={`${Style.formcontainer} ${Style.signin}`}>
                     <form>
-                        <h1>Sign In</h1>
-                        <div className={Style.socialicons}>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-google-plus-g"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-facebook-f"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-github"></i></a>
-                            <a href="#" className={Style.icon}><i className="fa-brands fa-linkedin-in"></i></a>
+                        <h2>Tipo de conta</h2>
+                        <div className={Style.typeofC}>
+                            <div className={typeOfConta === "aluno"? Style.selected : ""} onClick={()=>trocaTipoDeConta("aluno")}>
+                                <img src="../public/assets/aluno.png" alt="Logo Aluno" />
+                                <h3>Aluno</h3>
+                            </div>
+                            <div className={typeOfConta === "professor"? Style.selected : ""} onClick={()=>trocaTipoDeConta("professor")}>
+                                <img src="../public/assets/professor.png" alt="Logo Professor" />
+                                <h3>Professor</h3>
+                            </div>
+                            <div className={typeOfConta === "admin"? Style.selected : ""} onClick={()=>trocaTipoDeConta("admin")}>
+                                <img src="../public/assets/admin.png" alt="Logo admin" />
+                                <h3>Admin</h3>
+                            </div>
                         </div>
-                        <span>or use your email password</span>
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
-                        <a href="#">Forget Your Password?</a>
-                        <button>Sign In</button>
+                        <a href="#">esqueceu sua senha?</a>
+                        <button>Entrar</button>
                     </form>
                 </div>
 
@@ -52,14 +53,14 @@ const LoginPage = () => {
                 <div className={`${Style.togglecontainer}`}>
                     <div className={Style.toggle}>
                         <div className={`${Style.togglepanel} ${Style.toggleleft}`}>
-                            <h1>Welcome Back!</h1>
-                            <p>Enter your personal details to use all of site features</p>
+                            <h1>Bem vindo de volta!</h1>
+                            <p>Se você já tem uma conta click em sing in</p>
                             <button className={Style.hidden} onClick={()=>trocar()}>Sign In</button>
                         </div>
                         <div className={`${Style.togglepanel} ${Style.toggleright}`}>
-                            <h1>Hello, Friend!</h1>
-                            <p>Register with your personal details to use all of site features</p>
-                            <button className={Style.hidden} onClick={()=> trocar()} id="register">Sign Up</button>
+                            <h2>Olá, Amigo(a)!</h2>
+                            <p>Entre em Contato com nossos atendendes para realizar o cadastro</p>
+                            <button className={Style.hidden} onClick={()=> trocar()} id="register">Cadastrar</button>
                         </div>
                     </div>
                 </div>
